2d9ce67d6716c9d7e24a37cd0aac3fbbadceffdc
Amar SEN <amarsen2012@gmail.com>
2017-04-11 20:49:51 +0530

    Update Posts.php

--- MVC/App/Controllers/Posts.php
+++ MVC/App/Controllers/Posts.php
@@ -3,14 +3,14 @@
 namespace App\Controllers;
 
 class Posts extends \Core\Controller{
-	public function index(){
+	public function indexAction(){
 		echo "Hello I am in <b>index</b> action in <b>Posts</b> Controller";
 		// echo '<p>Query String Parameters:<pre>'. htmlspecialchars(print_r($_GET,true)).'<pre></p>';
 	}
-	public function addNew(){
+	public function addNewAction(){
 		echo "Hello I am in <b>addNew</b> action in <b>Posts</b> Controller";
 	}
-	public function edit(){
+	public function editAction(){
 		echo "Hello I am in <b>edit</b> action in <b>Posts</b> Controller";
 		echo '<p>Route Parameters:<pre>'. htmlspecialchars(print_r($this->route_params,true)).'<pre></p>';
 	}
