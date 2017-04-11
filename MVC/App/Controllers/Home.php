2591f6a37702cfee52c6898616993ecc1a4899eb
Amar SEN <amarsen2012@gmail.com>
2017-04-11 21:18:28 +0530

    Update Home.php

--- MVC/App/Controllers/Home.php
+++ MVC/App/Controllers/Home.php
@@ -2,10 +2,13 @@
 
 namespace App\Controllers;
 
+use \Core\View;
+
 class Home extends \Core\Controller{
 
 	public function indexAction(){
-		echo "Hello from <b>index</b> action in <b>Home</b> Controller";
+		// echo "Hello from <b>index</b> action in <b>Home</b> Controller";
+		View::render('Home/index.php'); //Scope resolution for calling static methods
 	}
 }
 
