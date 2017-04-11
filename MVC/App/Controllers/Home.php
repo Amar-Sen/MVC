acf90a50f0564fbfc12f6f900d43db6ad38a2f6e
Amar SEN <amarsen2012@gmail.com>
2017-04-11 20:50:20 +0530

    Update Home.php

--- MVC/App/Controllers/Home.php
+++ MVC/App/Controllers/Home.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 class Home extends \Core\Controller{
 
-	public function index(){
+	public function indexAction(){
 		echo "Hello from <b>index</b> action in <b>Home</b> Controller";
 	}
 }
