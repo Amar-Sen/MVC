103e0cf81b64ab213dcffa357035dbee51409aa9
Amar SEN <amarsen2012@gmail.com>
2017-04-12 17:05:39 +0530

    Update Posts.php

--- MVC/App/Controllers/Posts.php
+++ MVC/App/Controllers/Posts.php
@@ -3,12 +3,14 @@
 namespace App\Controllers;
 
 use \Core\View;
+use App\Model\Post;
 
 class Posts extends \Core\Controller{
 	public function indexAction(){
 		// echo "Hello I am in <b>index</b> action in <b>Posts</b> Controller";
 		// echo '<p>Query String Parameters:<pre>'. htmlspecialchars(print_r($_GET,true)).'<pre></p>';
-		View::renderTemplate('Posts/index.html');
+		$posts = Post::getAll();
+		View::renderTemplate('Posts/index.html',['posts'=>$posts]);
 	}
 	public function addNewAction(){
 		echo "Hello I am in <b>addNew</b> action in <b>Posts</b> Controller";
