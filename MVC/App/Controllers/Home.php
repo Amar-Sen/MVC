11e44d832c25a048a2706168a466db99358cdfec
Amar SEN <amarsen2012@gmail.com>
2017-04-12 11:11:40 +0530

    Update Home.php

--- MVC/App/Controllers/Home.php
+++ MVC/App/Controllers/Home.php
@@ -8,7 +8,8 @@ class Home extends \Core\Controller{
 
 	public function indexAction(){
 		// echo "Hello from <b>index</b> action in <b>Home</b> Controller";
-		View::render('Home/index.php'); //Scope resolution for calling static methods
+		/*View::render('Home/index.php',['name'=>'Dave','colors'=>['Green','Red','Blue']]); //Scope resolution for calling static methods*/
+		View::renderTemplate('Home/index.html',['name'=>'Dave','colors'=>['Green','Red','Blue']]); //Scope resolution for calling static methods
 	}
 }
 
